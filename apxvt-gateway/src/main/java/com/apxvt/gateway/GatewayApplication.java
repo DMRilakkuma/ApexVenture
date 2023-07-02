9f0d6c8e23c3bb58795199979f3d4a3587191288
王兵 <1182002995@qq.com>
2023-07-02 13:28:06 +0800

    环境变量配置修改2

--- apxvt-gateway/src/main/java/com/apxvt/gateway/GatewayApplication.java
+++ apxvt-gateway/src/main/java/com/apxvt/gateway/GatewayApplication.java
@@ -8,15 +8,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 
-@ComponentScan("com.pydance")
+@ComponentScan("com.apxvt")
 @SpringBootApplication
 public class GatewayApplication {
-    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+	private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(GatewayApplication.class);
-        Environment env = app.run(args).getEnvironment();
-        LOG.info("启动成功！！");
-        LOG.info("网关地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
-    }
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("网关地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
 }
